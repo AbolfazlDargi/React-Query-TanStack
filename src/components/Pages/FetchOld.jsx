2ca162aa6592a71d55691423b658f55cf8142247
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-09-17 17:48:19 +0330

    feat: Adding QueryClient and QueryClientProvider in React

--- src/components/Pages/FetchOld.jsx
+++ src/components/Pages/FetchOld.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import { fetchPosts } from "../../Api/api";
+
 export const FetchOld = () => {
-  return <h1>Hello FetchOld</h1>;
+  const [posts, setPosts] = useState([]);
+
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      console.log(res);
+      res.status == 200 ? setPosts(res.data) : [];
+    } catch (error) {
+      console.log(error);
+      return []
+    }
+  };
+
+  useEffect(() => {
+    getPostsData();
+  }, []);
+
+  return (
+    <div>
+      <ul
+        className="section-accordion">
+        {posts?.map((curElem) => {
+          return (
+            <li>
+              <p>{curElem.title}</p>
+              <p>{curElem.body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
