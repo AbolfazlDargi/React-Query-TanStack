29bed3bb61bb4ed6bc25197d4e9899b7e5ade3e0
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-09-17 14:54:57 +0330

    chore: update project files and minor fixes

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MainLayout } from "./components/layout/MainLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Home } from "./components/Pages/Home";
+import { FetchOld } from "./components/Pages/FetchOld";
+import { FetchRQ } from "./components/Pages/FetchRQ";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/trad",
+        element: <FetchOld />,
+      },
+      {
+        path: "/rq",
+        element: <FetchRQ />,
+      },
+    ],
+  },
+]);
+
+const app = () => {
+
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>;
+    </QueryClientProvider>
+  );
+};
+
+export default app;
